c8e405474c72d07aa82bf0a510de3dfb95690fff
sujoy007deb <59493587+sujoy007deb@users.noreply.github.com>
2020-01-15 20:55:51 -0500

    commit for run

---
+++ Test_v002/src/main/java/Test_v002/Test_v002/App.java
@@ -0,0 +1,13 @@
+package Test_v002.Test_v002;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Test_v002/src/test/java/test001/facebook.java
@@ -0,0 +1,35 @@
+package test001;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class facebook {
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver",
+				".\\Driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		//WebDriverWait d=new WebDriverWait(driver,20);
+		driver.get("https://facebook.com");
+		
+	}
+
+	@Test
+	public void firstTest()
+	{
+	String title = driver.getTitle();
+	System.out.println("title"+title);
+	driver.close();
+	}
+
+}
